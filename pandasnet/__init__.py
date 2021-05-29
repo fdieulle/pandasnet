9f5607a118d120be19893402287ad817e3ccb7ff
fdieulle <fabien.dieulle@gmail.com>
2021-05-29 15:02:10 -0400

    Make the package compatible with the last pythonnet version

--- pandasnet/__init__.py
+++ pandasnet/__init__.py
@@ -1,9 +1,10 @@
-import clr
-import os
+import os, sys
 
-lib_file = os.path.join(os.path.dirname(__file__), 'libs', 'PandasNet.dll')
-print(lib_file)
-clr.AddReference(lib_file)
+lib_folder = os.path.join(os.path.dirname(__file__), 'libs')
+sys.path.append(lib_folder)
+
+import clr
+clr.AddReference('PandasNet')
 
 from PandasNet import Codecs
 Codecs.Initialize(False)
