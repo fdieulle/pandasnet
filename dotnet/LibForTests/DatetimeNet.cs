3e743ccbacf449d37a43a2396a350526bfa78ebe
fdieulle <fabiendieull@hotmail.fr>
2023-09-06 21:57:35 -0400

    cover datetime codecs by unit tests

---
+++ dotnet/LibForTests/DatetimeNet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LibForTests
+{
+    public class DatetimeNet
+    {
+        public DateTime DateTimePassThrough(DateTime x) => x;
+
+        public TimeSpan TimeSpanPassThrough(TimeSpan x) => x;
+    }
+}
---
+++ tests/test_datetime_codecs.py
@@ -0,0 +1,32 @@
+import pandasnet
+from datetime import datetime, date, timedelta, time
+
+from LibForTests import DatetimeNet as dtnet
+
+
+def test_datetime():
+    now = datetime.now()
+    result = dtnet.DateTimePassThrough(now)
+    assert result == now
+
+def test_date():
+    today = datetime.today()
+    result = dtnet.DateTimePassThrough(today)
+
+    # Convert date to datetime
+    today = datetime.combine(today, datetime.min.time())
+    assert result == today
+
+def test_timedelta():
+    td = timedelta(days=1, hours=2, minutes=12, seconds=24, milliseconds=123)
+    result = dtnet.TimeSpanPassThrough(td)
+
+    assert result == td
+
+def test_time():
+    now = datetime.time()
+    result = dtnet.TimeSpanPassThrough(now)
+
+    # convert time to timedelta
+    now = datetime.combine(date.min, now) - datetime.min
+    assert result == now
